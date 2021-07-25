36ba6521b98a98f0212f02e2b89b0e28f508bc6e
Aleksandr Aladin <aleksandr_aladin@outlook.com>
2021-07-25 15:37:13 +0300

    feat: initially setup project

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     node: true,
   },
   extends: [
-    'plugin:vue/essential',
+    'plugin:vue/recommended',
     '@vue/airbnb',
   ],
   parserOptions: {
@@ -13,6 +13,8 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // vue specific rules
+    'vue/script-indent': ['error', 2, { baseIndent: 1, switchCase: 1 }]
   },
   overrides: [
     {
@@ -24,5 +26,13 @@ module.exports = {
         jest: true,
       },
     },
+    {
+      files: [
+        '*.vue'
+      ],
+      rules: {
+        indent: 'off',
+      }
+    }
   ],
 };
